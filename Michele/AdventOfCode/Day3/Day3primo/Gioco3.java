474fdb6fe5cd76ab3ecdb7e00d229c6082712bc4
mich9797 <michele.tha97@gmail.com>
2025-02-24 22:51:22 +0100

    corretto import inutili AOC

--- Michele/AdventOfCode/Day3/Day3primo/Gioco3.java
+++ Michele/AdventOfCode/Day3/Day3primo/Gioco3.java
@@ -1,17 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 public class Gioco3{
    
     public static void main(String[] args){
--- Michele/AdventOfCode/Day3/Day3secondo/Gioco3secondo.java
+++ Michele/AdventOfCode/Day3/Day3secondo/Gioco3secondo.java
@@ -1,18 +1,8 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Gioco3secondo{
    
