6cb0425fa823330332b6e4b07b5f5b6d50270371
mich9797 <michele.tha97@gmail.com>
2025-02-24 21:58:09 +0100

    ultimato AOC 3 parte due leggendo tutto il testo in un blocco unico

--- Michele/AdventOfCode/Day3/Day3secondo/Gioco3secondo.java
+++ Michele/AdventOfCode/Day3/Day3secondo/Gioco3secondo.java
@@ -3,6 +3,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,55 +19,39 @@ public class Gioco3secondo{
     public static void main(String[] args){
 
         try {
-            var src = new BufferedReader(new InputStreamReader(new FileInputStream("input3.txt")));
 
-        int somma = src.lines()
-                        .flatMap(riga -> {
-                            Pattern pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
-                            Matcher match = pattern.matcher(riga);
+        String testo = Files.readString(Paths.get("input3.txt"));
+        int somma = 0;
+                        
+        Pattern pattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)|don't\\(\\)|do\\(\\)");
+        Matcher match = pattern.matcher(testo); 
+    
+        boolean conta = true;
 
-                            Pattern patDont = Pattern.compile("don't");
-                            Matcher dontMatch = patDont.matcher(riga);    
-                            
-                            Pattern patDo = Pattern.compile("do");
-                            Matcher doMatch = patDo.matcher(riga); 
-                           
-                            List<Integer> trovati = new ArrayList<>();
-                            
-                            int x = 0;
-                            boolean calcola = true;
-                                while(match.find(x) && x < riga.length() ){ // se trovo un elemento che corrisponde
-                                        if(calcola){
-                                            String matchGroup = match.group();   // lo raccolgo
-                                            String[] numeri = matchGroup.substring(4, matchGroup.length() - 1).split(",");  //separo i due numeri che mi interessano che sono letti ancora in stringhe
-                                            int num1 = Integer.parseInt(numeri[0]);  // trasformo in interi
-                                            int num2 = Integer.parseInt(numeri[1]);  // trasformo in interi
-                                            trovati.add(num1 * num2);//aggiungo alla mia lista solo il risultato della moltriplicazione tra i due numeri 
-                                        }
-                                        x = match.end();
-                                        if(dontMatch.find()){
-                                            
-                                        }
-                                    
-                                }
-                            return trovati.stream();   //passo stream lista come return
-                            
-                        })
-                        .mapToInt(Integer::intValue)
-                        .sum();  
-
+        while(match.find()){     //trovo elemento
+            String patternTrovato = match.group();
+            switch (patternTrovato) {
+                case "don't()":
+                    conta = false;
+                    break;
+                case "do()":
+                    conta = true;
+                    break;
+                default:
+                    if(conta){
+                        String[] numeri = patternTrovato.substring(4, patternTrovato.length() - 1).split(",");  //separo i due numeri che mi interessano che sono letti ancora in stringhe
+                        int num1 = Integer.parseInt(numeri[0]);  // trasformo in interi
+                        int num2 = Integer.parseInt(numeri[1]);  // trasformo in interi
+                        somma += num1 * num2;
+                    }
+                    break;
+            }
+        }
+        
         System.out.println(somma);
 
         } catch (Exception e) {
             // TODO: handle exception
-        }
-        
-          
-            
-            
-            
-
+        }          
     }
-
-
 }
